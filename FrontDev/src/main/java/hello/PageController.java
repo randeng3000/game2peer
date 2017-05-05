f711061372f79ce68dfbb1c93ec542a310c64863
randeng3000 <randeng3000@gmail.com>
2017-05-05 15:18:25 +0800

    ..

--- FrontDev/src/main/java/hello/PageController.java
+++ FrontDev/src/main/java/hello/PageController.java
@@ -91,32 +91,7 @@ public class PageController {
 	   
 	}
 	
-	@RequestMapping("/pages/t/{id:.+}")
-	public String requestPages1(HttpServletRequest request,	HttpServletResponse response, @PathVariable String id, Model model) throws Exception
-	{
-		//环境属性
-		//匿名用户
-		User user = null;
-		
-		user = new User();
-		user.setLoginName("test");
-		user.setGroupId(2);
-		model.addAttribute("__userInfo", user);
-		
-		AccessRight ac = new AccessRight();
-		ac.setDel(1);
-		ac.setGet(1);
-		ac.setModi(1);
-		ac.setPut(1);
-		model.addAttribute("__accessRight", ac);
-		
-		model.addAttribute("__content_page", "page1");
-
-		//页面特殊属性
-		model.addAttribute("pageObject", "test");
-		
-		return "register";
-	}
+	
 
 	@RequestMapping("/apis/g/{id:.+}")
 	public void requestApi(HttpServletRequest request,	HttpServletResponse response, @PathVariable String id) throws Exception
