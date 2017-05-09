6fd9cd75df580dfa8af0e363b3a8a4ebbf25b69a
randeng3000 <randeng3000@gmail.com>
2017-05-09 16:50:18 +0800

    ---

--- game2peer/target/classes/static/style/js/share.js
+++
@@ -1,7 +0,0 @@
-function copyToClipBoard(id)
-{
-   var clipBoardContent="";
-   clipBoardContent+=$(id).val();
-   window.clipboardData.setData("Text",clipBoardContent);
-   //alert("复制成功，请粘贴到你的QQ/MSN上推荐给你的好友");
-}
